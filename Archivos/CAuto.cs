94e02d700b378dceecb6ad633bc3b570069cc41f
Monarca117 <skyggemonarken@gmail.com>
2024-04-30 10:43:50 -0600

    Version 1.0.0.0

---
+++ Archivos/CAuto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    internal class CAuto
+    {
+        public double costo;
+        public string modelo;
+
+        public double Costo { get {return costo ;} set { costo = value;} }
+        public String Modelo { get { return modelo; } set { modelo = value; } }
+
+        public CAuto(string pModelo, double pCosto)
+        {
+            costo = pCosto;
+            modelo = pModelo;
+        }
+
+        public void MuestraInformacion()
+        {
+            Console.WriteLine("El costo de tu auto es: " + Costo);
+            Console.WriteLine("El modelod e tu auto es: " + Modelo);
+            Console.WriteLine("_____________________________________");
+        }
+    }
+}
---
+++ Archivos/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            String valor = "";
+
+            Console.WriteLine("1) Guardar Archivo,  2) Leer Archivo");
+            valor = Console.ReadLine();
+            opcion = Convert.ToInt32(valor);
+
+            if(opcion == 1 ) 
+            {
+                //Creamos el objeto CAuto
+                string modelo = "";
+                double costo = 0;
+
+                Console.WriteLine("Dame el modelo");
+                modelo = Console.ReadLine();
+
+                Console.WriteLine("Dame el costo");
+                valor = Console.ReadLine();
+                costo = Convert.ToDouble(valor);
+
+                CAuto miAuto = new CAuto(modelo, costo);
+
+                //Variables extra
+                int numero = 5;
+                bool acceso = false;
+                byte conteo = 120;
+
+                //Creamos el Stream
+                FileStream fs = new FileStream("MiArchivo.arc", FileMode.Create, FileAccess.Write, FileShare.None);
+
+                //Creamos el escritor
+                BinaryWriter writer = new BinaryWriter(fs);
+
+                //Ecribimos los atributos del objeto
+                //NO ESTAMOS SERIALIZANDO
+                writer.Write(miAuto.Modelo);
+                writer.Write(miAuto.Costo);
+
+                //Escribimos las variables
+                writer.Write(numero);
+                writer.Write(acceso);   
+                writer.Write(conteo);
+
+                fs.Close();
+            }
+
+            if (opcion == 2 )
+            {
+                //Leemos el objeto
+                Console.WriteLine(" - - - Leemos el objeto - - - ");
+
+                //Creamos el Stream
+                FileStream fs = new FileStream("MiArchivo.arc", FileMode.Open, FileAccess.Read, FileShare.None);
+
+                //Creamos el lector
+                BinaryReader lector = new BinaryReader(fs);
+
+                //Leemos en el mismo orden en que se escribio
+
+                string modelo = "";
+                double costo = 0;
+                int numero = 0;
+                bool acceso = true;
+                byte conteo = 0;
+
+                modelo = lector.ReadString();
+                costo = lector.ReadDouble();
+                CAuto miAuto = new CAuto(modelo, costo);
+
+                numero = lector.ReadInt32();
+                acceso = lector.ReadBoolean();
+                conteo = lector.ReadByte();
+
+                //Cerramos el stream
+                fs.Close();
+
+                //Imprimimos
+                Console.WriteLine("numero {0}" + numero);
+                Console.WriteLine("acceso {0}" + acceso);
+                Console.WriteLine("conteo {0}" + conteo);
+            }
+        }
+    }
+}
